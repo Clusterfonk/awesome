c3320bca827ef28763ef2797d8bd648afe2cdd1e
clusterfonk <clusterfonk@pm.me>
2025-02-13 18:01:56 +0100

    each screen will have a seperate instance of the bar

--- ui/bars/info/init.lua
+++ ui/bars/info/init.lua
@@ -8,13 +8,15 @@ local dpi = bt.xresources.apply_dpi
 local clock = require("ui.widgets.clock")
 
 
-return function(s, height, strut_offset)
-    local geometry = {top = strut_offset, left = dpi(bt.useless_gap,s) * 2}
-    geometry.bottom = geometry.top + height + 2 * dpi(bt.taglist_border_width, s)
+return function(args)
+    local s = args.screen
+
+    local geometry = {top = args.strut_offset, left = dpi(bt.useless_gap,s) * 2}
+    geometry.bottom = geometry.top + args.height + 2 * dpi(bt.taglist_border_width, s)
 
     local clock_widget = clock {
         screen = s,
-        format = "  %d %b %H:%M ", -- spaces prevent color bugging out
+        format = " %d %b %H:%M ", -- spaces prevent colors bugging out
         font = bt.font_bold,
         top = geometry.bottom + 2 * bt.useless_gap,
         left = geometry.left
@@ -23,18 +25,17 @@ return function(s, height, strut_offset)
     s.info_bar = awful.popup {
         screen = s,
         ontop = true,
-        height = height,
+        height = args.height,
         border_width = dpi(bt.taglist_border_width, s),
         border_color = bt.taglist_border_color,
         bg = bt.colors.background,
-
         widget = {
             layout = wibox.layout.align.horizontal,
             expand = "outside",
             {
                 layout = wibox.layout.fixed.horizontal,
                 spacing = dpi(10, s),
-                forced_height = height,
+                forced_height = args.height,
                 clock_widget,
             }
         }
@@ -46,21 +47,25 @@ return function(s, height, strut_offset)
             margins = { top = geometry.top, left = geometry.left }
         })
 
-    client.connect_signal("focus", function(client)
-        if client.fullscreen then
-            s.info_bar.visible = false
+    client.connect_signal("property::fullscreen", function(client)
+        if s ~= client.screen then
+            return
         end
+
+        has_fullscreen_clients = false
+        for _, c in pairs(s.clients) do
+            has_fullscreen_clients = has_fullscreen_clients or c.fullscreen
+        end
+        s.info_bar.visible = not has_fullscreen_clients
     end)
 
-    client.connect_signal("property::fullscreen", function(client)
-        if client.fullscreen then
-            s.info_bar.visible = false
-        else
-            any_fullscreen = false
-            for _, c in pairs(s.clients) do
-                any_fullscreen = any_fullscreen or c.fullscreen
-            end
-            s.info_bar.visible = not any_fullscreen
+    s.info_bar:connect_signal("clear::popups", function()
+        clock_widget:get_widget().popup:emit_signal("popup::hide")
+    end)
+
+    client.connect_signal("button::press", function(_, _, _, button)
+        if button == 1 then
+            clock_widget:get_widget().popup:emit_signal("popup::hide")
         end
     end)
 end
--- ui/init.lua
+++ ui/init.lua
@@ -6,28 +6,34 @@ local dpi = require("beautiful").xresources.apply_dpi
 local taglist_bar = require(... .. ".bars.taglist")
 local info_bar = require(... .. ".bars.info")
 
-local SECONDARY_SCREEN <const> = 2
 
-test = {}
+local SECONDARY_SCREEN <const> = 2
 
 awful.screen.connect_for_each_screen(function(s)
     local taglist_bar_width = dpi(350, s)
     local bar_height = dpi(24, s)
 	local bar_offset = dpi(5, s)
 
-    --TODO: probably want them to build like all the widgets new()
-	taglist_bar(s, taglist_bar_width, bar_height, bar_offset)
+	taglist_bar {
+        screen = s,
+        height = bar_height,
+        width = taglist_bar_width,
+        strut_offset = bar_offset
+    }
 
-    info_bar(s, bar_height, bar_offset) -- returns only a function
+    info_bar {
+        screen = s,
+        height = bar_height,
+        strut_offset = bar_offset
+    }
 
 	if s.index == SECONDARY_SCREEN then
 	end
 
---    awful.mouse.append_global_mousebindings({
---        awful.button({}, 1, function()
---            print("click on screen: " .. awful.screen.index)
---            -- clear popups/widgets that should be closed when clicking anywhere
---        end)
---    })
 end)
 
+awful.mouse.append_global_mousebindings({
+    awful.button({}, 1, function()
+        mouse.screen.info_bar:emit_signal("clear::popups")
+    end)
+})

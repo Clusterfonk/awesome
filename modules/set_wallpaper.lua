5be0d6f39ed79b290d54b59d680591d4b849f82c
clusterfonk <clusterfonk@pm.me>
2025-02-13 18:01:30 +0100

    reworked

--- modules/set_wallpaper.lua
+++ modules/set_wallpaper.lua
@@ -1,26 +1,18 @@
 -- @license APGL-3.0 <https://www.gnu.org/licenses/>
 -- @author Clusterfonk <https://github.com/Clusterfonk>
 local awful = require("awful")
+local wibox = require("wibox")
 local bt = require("beautiful")
-local gears = require("gears")
 
 
-local function set_wallpaper(s)
-    if bt.wallpaper then
-        local wallpaper = bt.wallpaper
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
-end
-
-awful.screen.connect_for_each_screen(function(s)
-    set_wallpaper(s)
+screen.connect_signal("request::wallpaper", function(s)
+    awful.wallpaper {
+        screen = s,
+        widget = {
+            image = bt.wallpaper,
+            horizontal_fit_policy = "fit",
+            vertical_fit_policy = "fit",
+            widget = wibox.widget.imagebox,
+        }
+    }
 end)
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
-
-

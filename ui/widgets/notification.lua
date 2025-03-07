e803ad6738771e8d76bf78a730189de3c1960dae
clusterfonk <clusterfonk@pm.me>
2025-03-07 07:02:53 +0100

    split notification_builder into more appropriate files

--- ui/widgets/notification.lua
+++ ui/widgets/notification.lua
@@ -1,128 +1,69 @@
-local awful = require("awful")
-local ruled = require("ruled")
-local wibox = require("wibox")
-local bt = require("beautiful")
-local dpi = bt.xresources.apply_dpi
-local naughty = require("naughty")
-local gtimer = require("gears.timer")
-local nbox = require("ui.widgets.nbox")
-local debug = require("util.debug")
+--      @license APGL-3.0 <https://www.gnu.org/licenses/>
+--      @author clusterfonk
+local bt                   = require("beautiful")
+local dpi                  = bt.xresources.apply_dpi
+local naughty              = require("naughty")
+local wibox                = require("wibox")
 
 
-local capi = {
-    screen = screen
-}
-
--- NOTE: notification builder
-local notification_builder = { mt = {} }
-
-ruled.notification.connect_signal("request::rules", function() -- TODO: /configuration/ folder
-    --ruled.notification.append_rule({
-    --	rule = {
-    --		app_name = "blueman",
-    --	},
-    --	properties = {
-    --		icon = bt.get_svg_icon({ "blueman-device" }),
-    --	},
-    --})
-end)
-
-capi.screen.connect_signal("removed", function(s)
-    gtimer.delayed_call(function()
-        if by_pos_bar then -- NOTE: temp
-            by_pos_bar[s] = nil
-        end
-    end)
-end)
-
-function notification_builder.new(args)
-    by_pos_bar = args.by_pos_bar or {} -- NOTE: temp.
-
-    naughty.connect_signal("request::display", function(n)
-        local side_toggle = true -- will be turned into "left" "right"
-        local dnd = bt.notification_dnd
-
-        if dnd then
-            return n:destroy("dnd")
-        end
-        n.app_name = n.app_name or "System"
-        n.icon = n.icon or bt.icon.wlan
-        n.message = n.message or 'No message provided'
-        n.title = n.title or 'System Notification'
-
-        local notification = wibox.widget {
+return {
+    {
+        {
+            id            = "progress",
+            value         = 0,
+            forced_height = dpi(30),
+            forced_width  = 100,
+            paddings      = 0,
+            border_width  = 0,
+            color         = bt.notification_progress_color,
+            widget        = wibox.widget.progressbar,
+        },
+        {
             {
                 {
                     {
                         {
-                            {
-                                {
-                                    widget = naughty.widget.icon,
-                                },
-                                widget = wibox.container.place
-                            },
-                            widget = wibox.container.constraint,
-                            strategy = "max",
-                            width = dpi(20)
+                            widget = naughty.widget.icon,
                         },
-                        {
-                            {
-                                {
-                                    widget = naughty.widget.title,
-                                },
-                                widget = wibox.container.place
-                            },
-                            widget = wibox.container.constraint,
-                        },
-                        {
-                            {
-                                {
-                                    widget = naughty.widget.message,
-                                },
-                                widget = wibox.container.place
-                            },
-                            widget = wibox.container.constraint,
-                        },
-                        spacing = bt.useless_gap,
-                        layout = wibox.layout.fixed.horizontal,
+                        widget = wibox.container.place
                     },
-                    margins = {top = 2, bottom = 2, left = bt.useless_gap, right = bt.useless_gap},
-                    widget  = wibox.container.margin,
+                    widget = wibox.container.constraint,
+                    strategy = "max",
+                    width = dpi(20)
                 },
-                id     = "background_role",
-                widget = naughty.container.background,
+                {
+                    {
+                        {
+                            widget = naughty.widget.title,
+                        },
+                        widget = wibox.container.place
+                    },
+                    widget = wibox.container.constraint,
+                },
+                {
+                    {
+                        {
+                            {
+                                widget = naughty.widget.message,
+                            },
+                            layout = wibox.container.scroll.horizontal,
+                            max_size = 400,
+                            step_function = wibox.container.scroll.step_functions
+                                .waiting_nolinear_back_and_forth,
+                            speed = 25,
+                        },
+                        widget = wibox.container.place
+                    },
+                    widget = wibox.container.constraint,
+                },
+                spacing = bt.useless_gap,
+                layout = wibox.layout.fixed.horizontal,
             },
-            strategy = "exact",
-            height   = dpi(30),
-            widget   = wibox.container.constraint,
-        }
-
-        local screen = n.screen or awful.screen.focused
-        local side = "left"
-
-        if side_toggle then
-            n.max_width = dpi(444) - bt.useless_gap
-            -- check if one was already fired or have a toggle var
-            local box = nbox {
-                notification = n,
-                position = "top_right",
-                widget_template = notification
-            }
-        else
-            n.max_width = dpi(600)
-            local box = nbox {
-                notification = n,
-                position = "top_left",
-                widget_template = notification
-            }
-        end
-
-        side_toggle = not side_toggle
-    end)
-end
-
-function notification_builder.mt:__call(...)
-    return notification_builder.new(...)
-end
-
-return setmetatable(notification_builder, notification_builder.mt)
+            margins = { top = 2, bottom = 2, left = bt.useless_gap, right = bt.useless_gap },
+            widget  = wibox.container.margin,
+        },
+        widget = wibox.layout.stack
+    },
+    id     = "background_role",
+    widget = naughty.container.background,
+}
--- ui/widgets/notify.lua
+++ ui/widgets/notify.lua
@@ -3,7 +3,6 @@
 local gtable = require("gears.table")
 local gcolor = require("gears.color")
 local bt = require("beautiful")
-local dpi = bt.xresources.apply_dpi
 
 local ibutton = require("ui.widgets.ibutton")
 local center = require("ui.popups.notification_center")
@@ -14,19 +13,49 @@ local notify = { mt = {} }
 notify.icons = {
     normal = bt.icon.notification,
     normal_focus = gcolor.recolor_image(bt.icon.notification, bt.fg_focus),
-    active = bt.icon.notification_muted,
-    active_focus = gcolor.recolor_image(bt.icon.notification_muted, bt.fg_focus),
+    dnd = bt.icon.notification_dnd,
+    dnd_focus = gcolor.recolor_image(bt.icon.notification_dnd, bt.fg_focus),
+    unread_dnd = bt.icon.notification_unread_dnd,
+    unread_dnd_focus = gcolor.recolor_image(bt.icon.notification_unread_dnd, bt.fg_focus),
     unread = bt.icon.notification_unread,
     unread_focus = gcolor.recolor_image(bt.icon.notification_unread, bt.fg_focus)
 }
 
 local function on_press(self, _, _, btn, mods)
     if btn == 1 then
-        local instance = self._popup.instance
-        if instance then
-            --instance:show()
-        end
+        self:request_show()
     end
+end
+
+function notify:request_show()
+    local instance = self._popup.instance
+    if not instance then
+        instance = self._popup()
+    end
+
+    if instance._private.screen ~= self._private.screen then
+        instance._private.screen = self._private.screen
+        self._private.attach(instance)     -- auto detaches
+    end
+
+    instance:show()
+end
+
+function notify:get_icon()
+    local i = self._private.icons
+
+    if bt.notification_dnd then
+        -- unread
+        -- normal
+        return self._private.hovered and i.dnd_focus or i.dnd
+    elseif self._private.unread then
+        -- unread
+        -- normal
+        return self._private.hovered and i.unread_focus or i.unread
+    else
+        return self._private.hovered and i.normal_focus or i.normal
+    end
+
 end
 
 local function on_remove(self)
@@ -50,6 +79,7 @@ function notify.new(args)
 
     local ret = ibutton(args)
     gtable.crush(ret, notify, true)
+    ret._private.unread = false
 
     ret:connect_signal("button::press", on_press)
     ret:connect_signal("bar::removed", on_remove)
